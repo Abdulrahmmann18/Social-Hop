0c1c7bac222c57511d59342a9647e20c0114c05e
Abdelrahman Mohammed Farouk <abdulrahmmann18@gmail.com>
2025-12-23 04:37:15 +0200

    changes : 1- add images folder 2- install toastr package to use its service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,11 +5,17 @@ import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
+
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()),
+    provideAnimations(), // required animations providers
+    provideToastr(), // Toastr providers
   ]
 };
