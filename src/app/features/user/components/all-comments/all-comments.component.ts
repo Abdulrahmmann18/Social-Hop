82da1cb23f8a60c41fa9c43f3c85d8e903193d75
Abdelrahman Mohammed Farouk <abdulrahmmann18@gmail.com>
2025-12-23 05:05:43 +0200

    changes : 1- create interface for one post 2- assign allPosts Signal to the posts received from API

---
+++ src/app/features/user/components/all-comments/all-comments.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-all-comments',
+  imports: [],
+  templateUrl: './all-comments.component.html',
+  styleUrl: './all-comments.component.scss',
+})
+export class AllCommentsComponent {
+
+}
--- src/app/features/user/components/all-posts/all-posts.component.ts
+++ src/app/features/user/components/all-posts/all-posts.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { PostsService } from '../../services/posts.service';
+import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { PostsService } from '../../services/posts/posts.service';
+import { OnePost } from '../../interfaces/onePost/one-post';
 
 @Component({
   selector: 'app-all-posts',
@@ -12,10 +13,15 @@ export class AllPostsComponent implements OnInit {
 
   private postsService : PostsService = inject(PostsService);
 
+  allPosts : WritableSignal<OnePost[]> = signal<OnePost[]>([])
+  
   ngOnInit(): void {
     this.postsService.getAllPosts().subscribe({
       next : (res) => {
-        console.log(res);
+        // console.log(res);
+        this.allPosts.set(res.posts);
+        console.log(this.allPosts());
+        
       }
     }) 
   }
