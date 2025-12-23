c3a4bba3c5d191bf88fb1e864ead013876e90809
Abdelrahman Mohammed Farouk <abdulrahmmann18@gmail.com>
2025-12-23 04:42:13 +0200

    changes : create header interceptor to handle token in all APIs

---
+++ src/app/core/interceptors/header/header.interceptor.ts
@@ -0,0 +1,20 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { PlatformService } from '../../services/platform/platform.service';
+
+export const headerInterceptor: HttpInterceptorFn = (req, next) => {
+
+  let platformService : PlatformService = inject(PlatformService);
+  
+  if (platformService.isBrowserSide()) {
+    if (localStorage.getItem('userToken')) {
+      let tokenHeader : any = {token : localStorage.getItem('userToken')}
+      req = req.clone(
+        {
+          setHeaders : tokenHeader
+        }
+      )    
+    }
+  }
+  return next(req);
+};
